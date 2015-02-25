e4c3d7d5d7f11d6bc6b57836a7140ddb3e773ccd
Keith Mosher <kmosher@yelp.com>
2015-02-25 03:46:49 -0800

    Pull region/account from provider config

--- yelpaws/provider.go
+++ yelpaws/provider.go
@@ -17,22 +17,80 @@ var pvOnly = map[string]bool{"m1": true, "m2": true, "c1": true, "t1": true}
 
 func Provider() terraform.ResourceProvider {
 	provider := aws.Provider().(*schema.Provider)
-	instance := provider.ResourcesMap["aws_instance"]
-	ami := instance.Schema["ami"]
+	awsConfig := provider.Schema
+	awsConfig["account"] = &schema.Schema{
+		Type: schema.TypeString,
+		Required: true,
+		Description:
+			"The Yelp AWS account that is being used.\n" +
+			"Current known account are (dev, devb, stagea, stageb, prod)",
+		InputDefault: "dev",
+	}
+	instanceResource := provider.ResourcesMap["aws_instance"]
+	ami := instanceResource.Schema["ami"]
 	ami.Required = false
 	ami.Optional = true
 	ami.Computed = true
-	instance.Create = wrapCreate(instance.Create)
-	return provider
+
+	instanceResource.Create = wrapCreate(instanceResource.Create)
+	instanceResource.Read = wrapCRUD(instanceResource.Read)
+	instanceResource.Update = wrapCRUD(instanceResource.Update)
+	instanceResource.Delete = wrapCRUD(instanceResource.Delete)
+
+	return &schema.Provider{
+		Schema: awsConfig,
+		ResourcesMap: map[string]*schema.Resource{
+			"yelpaws_instance": instanceResource,
+		},
+		ConfigureFunc: wrapConfigure(provider.ConfigureFunc),
+	}
+}
+
+// TODO: Replace region/account with an AMIFetcher interface
+type YelpAWSClient struct {
+	Account string
+	Region string
+	*aws.AWSClient
+}
+
+
+func wrapConfigure(configure func(*schema.ResourceData) (interface{}, error)) func(*schema.ResourceData) (interface{}, error) {
+	wrapped := func(d *schema.ResourceData) (interface{}, error) {
+		awsClient, error := configure(d)
+		if error != nil {
+			return awsClient, error
+		}
+		client := YelpAWSClient{
+			Account: d.Get("account").(string),
+			Region: d.Get("region").(string),
+			AWSClient: awsClient.(*aws.AWSClient),
+		}
+		return &client, nil
+	}
+	return wrapped
+}
+
+func wrapCRUD(f func(*schema.ResourceData, interface{}) error) func(*schema.ResourceData, interface{}) error {
+	wrapped := func(d *schema.ResourceData, meta interface{}) error {
+		yelpClient := meta.(*YelpAWSClient)
+		return f(d, yelpClient.AWSClient)
+	}
+	return wrapped
 }
 
 func wrapCreate(create func(*schema.ResourceData, interface{}) error) func(*schema.ResourceData, interface{}) error {
 	wrapped := func(d *schema.ResourceData, meta interface{}) error {
+		yelpClient := meta.(*YelpAWSClient)
 		if _, ok := d.GetOk("ami"); !ok {
-			ami := getAMI("lucid", d.Get("instance_type").(string))
+			ami := getAMI(
+				"lucid",
+				d.Get("instance_type").(string),
+				yelpClient.Account,
+				yelpClient.Region,
+			)
 			d.Set("ami", ami)
 		}
-		return create(d, meta)
+		return create(d, yelpClient.AWSClient)
 	}
 	return wrapped
 }
@@ -45,10 +103,13 @@ func buildAmiUrl(osdist string, hvm bool, account string, region string) string 
 	return fmt.Sprintf(jenkinsUrlFmt, osdist, hvmSuffix) + fmt.Sprintf(artifactFmt, account, region)
 }
 
-func getAMI(osdist string, instanceType string) string {
-	// TODO: get account, region from provider config
-	url := buildAmiUrl(osdist, supportsHVM(instanceType), "dev", "us-west-1")
+func getAMI(osdist string, instanceType string, account string, region string) string {
+	url := buildAmiUrl(osdist, supportsHVM(instanceType), account, region)
 	resp, _ := http.Get(url)
+	// TODO: Smart things on error
+	if resp.StatusCode != 200 {
+		return "bad-ami"
+	}
 	defer resp.Body.Close()
 	ami_id, _ := ioutil.ReadAll(resp.Body)
 	return strings.TrimSpace(string(ami_id))
