23fe4bee560e16f3a4f80492c230810214344d4d
Tomas Doran <tdoran@yelp.com>
2015-03-28 15:55:51 -0700

    Build against my terraform fork

--- yelppack/build.sh
+++ yelppack/build.sh
@@ -5,6 +5,15 @@ version=$2
 iteration=$3
 
 go get ${project}
+pushd /go/src/github.com/hashicorp/terraform
+git remote add bobtfish https://github.com/bobtfish/terraform.git
+git fetch bobtfish
+git fetch --tags bobtfish
+git checkout 0.4.0-pre1
+popd
+pushd /go/bin
+go build ../src/terraform-provider-yelpaws
+popd
 mkdir /dist && cd /dist
 fpm -s dir -t deb --name ${project} \
     --iteration ${iteration} --version ${version} \
